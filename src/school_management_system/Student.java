73012093d9e6e80c895172e8d4cecf1af129abc9
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2024-10-20 19:50:17 +0530

    studentClass
    creating the student class

---
+++ src/school_management_system/Student.java
@@ -0,0 +1,23 @@
+package school_management_system;
+
+public class Student {
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
+    public Student(int id, String name, int grade){
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = 0;
+        this.feesTotal = 30000;
+    }
+
+    // we are not going to alter(change) the students name and id;
+
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+}
